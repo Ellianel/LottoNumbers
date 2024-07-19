0c7f9e600cb8cc5b58e8bf04cc731017a79124e4
Ellianel <michal93nieklan@gmail.com>
2024-07-19 10:19:16 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿Random rand = new();
+
+List<int> lottoNumbers = new List<int>();
+
+for (int i = 1; i <= 49; i++)
+{
+    lottoNumbers.Add(i);
+}
+
+int[] lottoWinners = new int[6];
+
+for(int i = 0; i < lottoWinners.Length; i++)
+{
+    int random = rand.Next(0, lottoNumbers.Count);
+    lottoWinners[i] = lottoNumbers[random];
+    lottoNumbers.RemoveAt(random);
+
+}
+
+foreach(int number in lottoWinners)
+{
+Console.WriteLine(number);
+}
